d4558fdfbe3d02a82ae2743d9f85aaf690ce25d2
Alvaro <alvarofdezg@proton.me>
2026-02-09 17:24:28 +0100

    Listado pelis

--- pages/dashboard-admin.php
+++ pages/dashboard-admin.php
@@ -1,19 +1,37 @@
 <?php
+require("./../scripts/bbdd.php");
 session_start();
 
 if (!isset($_SESSION["rol"]) || $_SESSION["rol"] != 1) {
     header("Location: ./../index.php?error=true");
 }
+$listaPelis = getPelis();
 
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Administrador</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="./../css/style.css">
+    <link rel="stylesheet" href="./../css/dashboard.css">
 </head>
+
 <body>
-    <h1>Bienvenido administrador <?php echo $_SESSION["userName"] ?></h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary position-fixed w-100 p-2 header">
+        <h1 class="navbar-brand m-0 mx-3"> <i class="fa-solid fa-film"></i> Videoclub online</h1>
+    </nav>
+    <div class="container-xl contenedor">
+
+        <h3>Bienvenido administrador <?php echo $_SESSION["userName"] ?></h3>
+        <br>
+        <?php print_r($listaPelis) ?>
+    </div>
+
 </body>
+
 </html>
--- scripts/bbdd.php
+++ scripts/bbdd.php
@@ -49,12 +49,47 @@ function createTables()
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(100) NOT NULL UNIQUE,
         password VARCHAR(100) NOT NULL,
-        rol VARCHAR(100) DEFAULT ('Por revisar'))";
+        rol VARCHAR(100) NOT NULL)";
+
+        $inserts = "INSERT INTO actores (nombre, apellidos, fotografía) VALUES
+        ('Leonardo', 'DiCaprio', 'dicaprio.jpg'),
+        ('Scarlett', 'Johansson', 'johansson.jpg'),
+        ('Brad', 'Pitt', 'pitt.jpg'),
+        ('Natalie', 'Portman', 'portman.jpg'),
+        ('Christian', 'Bale', 'bale.jpg'),
+        ('Edward', 'Norton', 'norton.jpg'),
+        ('Heath', 'Ledger', 'ledger.jpg'),
+        ('Mila', 'Kunis', 'kunis.jpg');
+
+
+        INSERT INTO peliculas (titulo, genero, pais, anyo, cartel) VALUES
+        ('Inception', 'Ciencia ficción', 'Estados Unidos', 2010, 'inception.jpg'),
+        ('Lost in Translation', 'Drama', 'Estados Unidos', 2003, 'lost_translation.jpg'),
+        ('Fight Club', 'Drama', 'Estados Unidos', 1999, 'fight_club.jpg'),
+        ('Black Swan', 'Thriller', 'Estados Unidos', 2010, 'black_swan.jpg'),
+        ('The Dark Knight', 'Acción', 'Estados Unidos', 2008, 'dark_knight.jpg');
+
+        INSERT INTO actuan (id_pelicula, id_actor) VALUES
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (3, 6),
+        (4, 4),
+        (5, 5),
+        (5, 7);
+
+        INSERT INTO usuarios (username, password, rol) VALUES
+        ('admin1', 'pass', '1'),
+        ('admin2', 'pass', '1'),
+        ('usuario1', 'pass', '0'),
+        ('usuario2', 'pass', '0'),
+        ('usuario3', 'pass', '0');";
 
         $bd->exec($peliculas);
         $bd->exec($actores);
         $bd->exec($actuan);
         $bd->exec($usuarios);
+        $bd->exec($inserts);
 
         $bd = null;
     } catch (PDOException $e) {
@@ -83,7 +118,7 @@ function login($userName, $password)
         echo '<p class="error-message">Error en la base de datos: <strong>' . $e->getMessage() . '</strong></p>';
     }
 }
-function listarPelis()
+function getPelis()
 {
     $conex = "mysql:host=127.0.0.1;dbname=videoclub";
     $user = "root";
@@ -91,14 +126,11 @@ function listarPelis()
 
     try {
         $bd = new PDO($conex, $user, $pass);
-        $userLogin = $bd->prepare("SELECT * FROM peliculas");
-        if ($userLogin->rowCount() == 1) {
-            $userData =  $userLogin->fetch(PDO::FETCH_ASSOC);
-            return $userData;
-        } else {
-            return false;
-        }
+        $sql = $bd->prepare("SELECT * FROM peliculas");
+        $sql->execute();
 
+        $pelisData =  $sql->fetchAll(PDO::FETCH_ASSOC);
+        return $pelisData;
         $bd = null;
     } catch (PDOException $e) {
         echo '<p class="error-message">Error en la base de datos: <strong>' . $e->getMessage() . '</strong></p>';
